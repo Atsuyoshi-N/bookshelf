19a5db9ce5332044923ed113237de396f1a2f25c
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2026-03-17 05:06:34 +0900

    feat: GitHubのリポジトリ名を変更

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.GITHUB_PAGES === "true" ? "/kindle-bookshelf" : "",
+  basePath: process.env.GITHUB_PAGES === "true" ? "/bookshelf" : "",
   images: {
     unoptimized: true,
   },
